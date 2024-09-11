2c4437daea55bfde765e9a34089215166a2b9be2
lyremote <liuyanoutsee@outlook.com>
2024-09-11 14:38:25 +0800

    [a]解决maven打包报错，<skip>true</skip>

--- boot_rabbit_consumer/src/main/java/org/lyflexi/boot_rabbit_consumer/BootRabbitConsumerApplication.java
+++ boot_rabbit_consumer/src/main/java/org/lyflexi/boot_rabbit_consumer/BootRabbitConsumerApplication.java
@@ -2,8 +2,11 @@ package org.lyflexi.boot_rabbit_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//解决common打包失败问题
+@ComponentScan(value = "org.lyflexi")
 public class BootRabbitConsumerApplication {
 
     public static void main(String[] args) {
--- boot_rabbit_producer/src/main/java/org/lyflexi/boot_rabbit_producer/BootRabbitProducerApplication.java
+++ boot_rabbit_producer/src/main/java/org/lyflexi/boot_rabbit_producer/BootRabbitProducerApplication.java
@@ -2,8 +2,11 @@ package org.lyflexi.boot_rabbit_producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//解决common打包失败问题
+@ComponentScan(value = "org.lyflexi")
 public class BootRabbitProducerApplication {
 
     public static void main(String[] args) {
